d52a9fd1452190b40246a69319b18b663bc587ae
jenobpj <jenobgit@gmail.com>
2021-09-28 00:10:45 -0400

    bodyparser added

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,8 +31,24 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' 
+});
+
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString(){
+  let randomCharacters='';
+  const characters='ABCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnopqrstuvwxy1234567890';
+  for(let i=0;i<6;i++){
+    let randomNumber=Math.floor(Math.random() * 56 +1);
+    randomCharacters +=characters[randomNumber]
+  }
+  return randomCharacters;
+
+}

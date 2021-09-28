aa40180d05c28b342557e5ea62f890e85bed8d10
jenobpj <jenobgit@gmail.com>
2021-09-28 00:19:46 -0400

    post urls tested

--- express_server.js
+++ express_server.js
@@ -27,13 +27,19 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const longURL= urlDatabase[req.params.shortURL];
+  if (longURL === undefined) {
+    res.redirect("/urls");
+    return;
+  }
   const templateVars = { shortURL: req.params.shortURL, longURL: longURL};
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' 
+  let shortURL=generateRandomString();
+  urlDatabase[shortURL]=req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
 });
 
 
